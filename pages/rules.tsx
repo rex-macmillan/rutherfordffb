19505e74b77a263c948a2f9ec84f1ef9ebebf00d
rex-macmillan <rex.macmillan@greenirony.com>
2025-07-20 17:50:42 -0400

    fixing styling

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -1,7 +1,7 @@
 export default function RulesPage() {
   return (
     <main style={{ padding: "2rem" }}>
-    <div style={{maxWidth:"800px",lineHeight:1.6}}>
+    <div style={{maxWidth:"800px"}}>
       <h1>Keeper Rules Guide</h1>
 
  
